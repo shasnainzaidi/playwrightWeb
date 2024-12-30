a17872aea91ec79f682802b381cd737c8aba3a13
shasnainzaidi <hasnain.xaidi@outlook.com>
2024-12-30 16:12:36 +0500

    Add TestNG for running the testcases

--- src/test/java/first/playwright/firstPlaywrightTest.java
+++ src/test/java/first/playwright/firstPlaywrightTest.java
@@ -4,11 +4,19 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class firstPlaywrightTest {
-        public static void main(String[] args) {
-            pageObjects PageObjects = new pageObjects();
-            try {
+
+private pageObjects PageObjects;
+     @BeforeClass
+    public void setUp() {
+         PageObjects = new pageObjects();
+     }
+         @Test
+         public void emailLogin(){
                 String appUrl = "http://olx.com.pk";
             PageObjects.navigateTo(appUrl);
             PageObjects.clickLoginButton();
@@ -17,9 +25,11 @@ public class firstPlaywrightTest {
             PageObjects.enterPassword("qwerty10@");
             PageObjects.clickSubmitButton();
             }
-            finally {
-                PageObjects.closeBrowser();
-            }
-        }
+
+    @AfterClass
+    public void tearDown() {
+        // Close the browser
+        PageObjects.closeBrowser();
     }
+        }
 
